da8c72d7050403c413156c1515237b10090e226c
korotkov52 <dkorotkov52@yandex.ru>
2019-04-03 22:34:36 +0300

    Initial commit

---
+++ app/src/main/java/ru/korotkov/electronicdocuments/AuthActivity.kt
@@ -0,0 +1,34 @@
+package ru.korotkov.electronicdocuments
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_authorization.*
+
+class AuthActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        fun newIntent(context: Context): Intent =
+            Intent(context, AuthActivity::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+        sing.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.sing -> login()
+        }
+    }
+
+    private fun login() {
+        finish()
+    }
+
+}
