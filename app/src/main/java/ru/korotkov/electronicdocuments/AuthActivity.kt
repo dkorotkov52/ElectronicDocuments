c0a38c97e4aceb043b9115338e4390b3c1ba4092
korotkov52 <dkorotkov52@yandex.ru>
2019-04-04 23:16:51 +0300

    hello text

--- app/src/main/java/ru/korotkov/electronicdocuments/AuthActivity.kt
+++ app/src/main/java/ru/korotkov/electronicdocuments/AuthActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_authorization.*
 
@@ -28,7 +27,9 @@ class AuthActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun login() {
-        finish()
+        if(login.text!!.equals("89000000000") && password.text!!.equals("12345678")) {
+            finish()
+        }
     }
 
 }
