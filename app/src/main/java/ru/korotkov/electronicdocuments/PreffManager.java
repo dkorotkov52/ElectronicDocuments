7222077da981c615252b201e8c0e7d923ebcaf9e
korotkov52 <dkorotkov52@yandex.ru>
2019-04-10 19:51:40 +0300

    PreffManager

--- app/src/main/java/ru/korotkov/electronicdocuments/PreffManager.java
+++ app/src/main/java/ru/korotkov/electronicdocuments/PreffManager.java
@@ -4,10 +4,25 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class PreffManager {
-
     static SharedPreferences pref;
     static SharedPreferences.Editor editor;
     static Context _context;
 
+    public static final String PREF_NAME = "electronicDocuments";
+
+    public static void init(Context context) {
+        _context = context;
+        pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    public static void setString(String tag, String value) {
+        editor.putString(tag, value);
+        editor.commit();
+    }
+
+    public static String getString(String tag) {
+        return pref.getString(tag, "");
+    }
 
 }
